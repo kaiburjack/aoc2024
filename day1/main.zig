bbd1aa7fb1ede5af2a6bdd96112d55340eed4668
Kai Burjack <kai.burjack@hbt.de>
2024-12-01 11:30:35 +0100

    refactor: use more idiomatic Zig syntax

--- day1/main.zig
+++ day1/main.zig
@@ -1,52 +1,48 @@
 const std = @import("std");
 
-fn part1(leftSlice: []i64, rightSlice: []i64) u64 {
+fn part1(left_slice: []const i64, right_slice: []const i64) u64 {
     var sum : u64 = 0;
-    for (leftSlice, rightSlice) |left, right| {
+    for (left_slice, right_slice) |left, right| {
         sum += @abs(left - right);
     }
     return sum;
 }
 
-fn part2(leftSlice: []i64, rightSlice: []i64) u64 {
+fn part2(left_slice: []const i64, right_slice: []const i64) u64 {
     var sum : u64 = 0;
-    var rightIndex : usize = 0;
-    for (leftSlice) |left| {
-        while (rightIndex < rightSlice.len and rightSlice[rightIndex] < left) {
-            rightIndex += 1;
+    var right_index : usize = 0;
+    for (left_slice) |left| {
+        while (right_index < right_slice.len and right_slice[right_index] < left) : (right_index += 1) {
         }
-        while (rightIndex < rightSlice.len and rightSlice[rightIndex] == left) {
+        while (right_index < right_slice.len and right_slice[right_index] == left) : (right_index += 1) {
             sum += @intCast(left);
-            rightIndex += 1;
         }
     }
     return sum;
 }
 
 pub fn main() !void {
-    var alloc = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = alloc.allocator();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
     const file = try std.fs.cwd().openFile("input.txt", .{});
     const file_size = (try file.stat()).size;
     const input = try file.readToEndAlloc(allocator, file_size);
-    defer allocator.free(input);
     file.close();
-    var lines = std.mem.tokenizeAny(u8, input, "\n");
-    var leftInts = std.ArrayList(i64).init(allocator);
-    var rightInts = std.ArrayList(i64).init(allocator);
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    var left_ints = std.ArrayList(i64).init(allocator);
+    var right_ints = std.ArrayList(i64).init(allocator);
     while (lines.next()) |line| {
-        var leftAndRight = std.mem.tokenizeScalar(u8, line, ' ');
-        const left = leftAndRight.next().?;
-        const right = leftAndRight.next().?;
-        try leftInts.append(try std.fmt.parseInt(i64, left, 10));
-        try rightInts.append(try std.fmt.parseInt(i64, right, 10));
+        var left_and_right = std.mem.tokenizeScalar(u8, line, ' ');
+        const left = left_and_right.next().?;
+        const right = left_and_right.next().?;
+        try left_ints.append(try std.fmt.parseInt(i64, left, 10));
+        try right_ints.append(try std.fmt.parseInt(i64, right, 10));
     }
-    const leftSlice = try leftInts.toOwnedSlice();
-    const rightSlice = try rightInts.toOwnedSlice();
-    defer allocator.free(leftSlice);
-    defer allocator.free(rightSlice);
-    std.mem.sort(i64, leftSlice, {}, comptime std.sort.asc(i64));
-    std.mem.sort(i64, rightSlice, {}, comptime std.sort.asc(i64));
-    std.debug.print("{}\n", .{part1(leftSlice, rightSlice)});
-    std.debug.print("{}\n", .{part2(leftSlice, rightSlice)});
+    const left_slice = try left_ints.toOwnedSlice();
+    const right_slice = try right_ints.toOwnedSlice();
+    std.mem.sort(i64, left_slice, {}, std.sort.asc(i64));
+    std.mem.sort(i64, right_slice, {}, std.sort.asc(i64));
+    std.debug.print("{}\n", .{part1(left_slice, right_slice)});
+    std.debug.print("{}\n", .{part2(left_slice, right_slice)});
 }
