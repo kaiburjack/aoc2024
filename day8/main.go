3d199d24501edd392cb3bd2d63727ee8138dad82
Kai Burjack <kai.burjack@hbt.de>
2024-12-08 07:16:09 +0100

    refactor: collapse some variable declarations and remove coord struct

--- day8/main.go
+++ day8/main.go
@@ -5,23 +5,19 @@ import (
 	"os"
 )
 
-type coord struct {
-	x, y int
-}
-
-func extrude(minM int, maxM int, f coord, dx int, dy int, field [][]byte, uniquePoints map[coord]struct{}) {
-	for m, x0, y0 := minM, f.x+minM*dx, f.y+minM*dy; m <= maxM && x0 >= 0 && x0 < len(field[0]) && y0 >= 0 && y0 < len(field); m, x0, y0 = m+1, x0+dx, y0+dy {
-		uniquePoints[coord{x0, y0}] = struct{}{}
+func extrude(minM int, maxM int, f [2]int, dx int, dy int, field [][]byte, uniquePoints map[[2]int]struct{}) {
+	for m, x0, y0 := minM, f[0]+minM*dx, f[1]+minM*dy; m <= maxM && x0 >= 0 && x0 < len(field[0]) && y0 >= 0 && y0 < len(field); m, x0, y0 = m+1, x0+dx, y0+dy {
+		uniquePoints[[2]int{x0, y0}] = struct{}{}
 	}
 }
 
-func computeUniqueAntinodes(freqs map[byte][]coord, field [][]byte, minM, maxM int) map[coord]struct{} {
-	uniquePoints := make(map[coord]struct{})
+func computeUniqueAntinodes(freqs map[byte][][2]int, field [][]byte, minM, maxM int) map[[2]int]struct{} {
+	uniquePoints := make(map[[2]int]struct{})
 	for _, fp := range freqs {
 		for fi := 0; fi < len(fp); fi++ {
 			for fj := fi + 1; fj < len(fp); fj++ {
-				extrude(minM, maxM, fp[fi], fp[fi].x-fp[fj].x, fp[fi].y-fp[fj].y, field, uniquePoints)
-				extrude(minM, maxM, fp[fj], fp[fj].x-fp[fi].x, fp[fj].y-fp[fi].y, field, uniquePoints)
+				extrude(minM, maxM, fp[fi], fp[fi][0]-fp[fj][0], fp[fi][1]-fp[fj][1], field, uniquePoints)
+				extrude(minM, maxM, fp[fj], fp[fj][0]-fp[fi][0], fp[fj][1]-fp[fi][1], field, uniquePoints)
 			}
 		}
 	}
@@ -30,18 +26,15 @@ func computeUniqueAntinodes(freqs map[byte][]coord, field [][]byte, minM, maxM i
 
 func main() {
 	file, _ := os.Open("real.txt")
-	scanner := bufio.NewScanner(file)
-	field := make([][]byte, 0)
-	freqs := make(map[byte][]coord)
+	scanner, field, freqs := bufio.NewScanner(file), make([][]byte, 0), make(map[byte][][2]int)
 	for scanner.Scan() {
-		line := scanner.Text()
-		for i, c := range line {
+		for i, c := range scanner.Bytes() {
 			if c == '.' {
 				continue
 			}
-			freqs[byte(c)] = append(freqs[byte(c)], coord{i, len(field)})
+			freqs[c] = append(freqs[c], [2]int{i, len(field)})
 		}
-		field = append(field, []byte(line))
+		field = append(field, scanner.Bytes())
 	}
 	println(len(computeUniqueAntinodes(freqs, field, 1, 1)))
 	println(len(computeUniqueAntinodes(freqs, field, 0, 100000)))
