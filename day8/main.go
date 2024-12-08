e8c447d395f5dd80ca1aa22d444710855e3e8848
Kai Burjack <kai.burjack@hbt.de>
2024-12-08 12:32:55 +0100

    refactor: use width/height instead of the field
    we only ever need the width and height of the field,
    not the actual field itself.

--- day8/main.go
+++ day8/main.go
@@ -5,19 +5,19 @@ import (
 	"os"
 )
 
-func extrude(minM int, maxM int, f [2]int, dx int, dy int, field [][]byte, uniquePoints map[[2]int]struct{}) {
-	for m, x0, y0 := minM, f[0]+minM*dx, f[1]+minM*dy; m <= maxM && x0 >= 0 && x0 < len(field[0]) && y0 >= 0 && y0 < len(field); m, x0, y0 = m+1, x0+dx, y0+dy {
+func extrude(minM, maxM, dx, dy, w, h int, f [2]int, uniquePoints map[[2]int]struct{}) {
+	for m, x0, y0 := minM, f[0]+minM*dx, f[1]+minM*dy; m <= maxM && x0 >= 0 && x0 < w && y0 >= 0 && y0 < h; m, x0, y0 = m+1, x0+dx, y0+dy {
 		uniquePoints[[2]int{x0, y0}] = struct{}{}
 	}
 }
 
-func computeUniqueAntinodes(freqs map[byte][][2]int, field [][]byte, minM, maxM int) map[[2]int]struct{} {
+func computeUniqueAntinodes(freqs map[byte][][2]int, w, h, minM, maxM int) map[[2]int]struct{} {
 	uniquePoints := make(map[[2]int]struct{})
 	for _, fp := range freqs {
 		for fi := 0; fi < len(fp); fi++ {
 			for fj := fi + 1; fj < len(fp); fj++ {
-				extrude(minM, maxM, fp[fi], fp[fi][0]-fp[fj][0], fp[fi][1]-fp[fj][1], field, uniquePoints)
-				extrude(minM, maxM, fp[fj], fp[fj][0]-fp[fi][0], fp[fj][1]-fp[fi][1], field, uniquePoints)
+				extrude(minM, maxM, fp[fi][0]-fp[fj][0], fp[fi][1]-fp[fj][1], w, h, fp[fi], uniquePoints)
+				extrude(minM, maxM, fp[fj][0]-fp[fi][0], fp[fj][1]-fp[fi][1], w, h, fp[fj], uniquePoints)
 			}
 		}
 	}
@@ -36,6 +36,6 @@ func main() {
 		}
 		field = append(field, scanner.Bytes())
 	}
-	println(len(computeUniqueAntinodes(freqs, field, 1, 1)))
-	println(len(computeUniqueAntinodes(freqs, field, 0, 100000)))
+	println(len(computeUniqueAntinodes(freqs, len(field[0]), len(field), 1, 1)))
+	println(len(computeUniqueAntinodes(freqs, len(field[0]), len(field), 0, 100000)))
 }
