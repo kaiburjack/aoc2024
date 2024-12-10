53061350d847cf5d16c4b7e8cacfced4aba22cac
Kai Burjack <kai.burjack@hbt.de>
2024-12-10 09:32:14 +0100

    refactor: use loop for all four directions

--- day10/main.go
+++ day10/main.go
@@ -6,22 +6,16 @@ import (
 )
 
 func walk(x, y int, v byte, grid [][]byte, reachable map[[2]int]struct{}) int {
-	distinct := 0
 	if v == '9' {
 		reachable[[2]int{x, y}] = struct{}{}
 		return 1
 	}
-	if x > 0 && grid[y][x-1] == v+1 {
-		distinct += walk(x-1, y, v+1, grid, reachable)
-	}
-	if x < len(grid[0])-1 && grid[y][x+1] == v+1 {
-		distinct += walk(x+1, y, v+1, grid, reachable)
-	}
-	if y > 0 && grid[y-1][x] == v+1 {
-		distinct += walk(x, y-1, v+1, grid, reachable)
-	}
-	if y < len(grid)-1 && grid[y+1][x] == v+1 {
-		distinct += walk(x, y+1, v+1, grid, reachable)
+	distinct := 0
+	dirs := [4][2]int{{0, -1}, {0, 1}, {-1, 0}, {1, 0}}
+	for _, d := range dirs {
+		if x+d[0] >= 0 && x+d[0] < len(grid[0]) && y+d[1] >= 0 && y+d[1] < len(grid) && grid[y+d[1]][x+d[0]] == v+1 {
+			distinct += walk(x+d[0], y+d[1], v+1, grid, reachable)
+		}
 	}
 	return distinct
 }
@@ -32,17 +26,16 @@ func main() {
 	var grid [][]byte
 	var startingPoints [][2]int
 	for r.Scan() {
-		var row []byte
-		for _, c := range r.Bytes() {
-			row = append(row, c)
+		row := r.Bytes()
+		for x, c := range row {
 			if c == '0' {
-				startingPoints = append(startingPoints, [2]int{len(row) - 1, len(grid)})
+				startingPoints = append(startingPoints, [2]int{x, len(grid)})
 			}
 		}
 		grid = append(grid, row)
 	}
 
-	sumPart2, sumPart1 := 0, 0
+	sumPart1, sumPart2 := 0, 0
 	for _, sp := range startingPoints {
 		reachable := make(map[[2]int]struct{})
 		sumPart2 += walk(sp[0], sp[1], '0', grid, reachable)
