e7d968066ccf495ea0b1e3bcab4a93e942df568a
Kai Burjack <kai.burjack@hbt.de>
2024-12-18 13:57:39 +0100

    Add day16 part2 solution

--- day16/main.go
+++ day16/main.go
@@ -14,22 +14,22 @@ type coord struct {
 }
 
 type node struct {
-	neighbors [3]*node
-	cost      uint64
-	dcost     uint64
-	deadEnd   bool
-	end       bool
+	neighbors   [3]*node
+	prev        []*node
+	c           coord
+	cost, dcost uint
+	end, seen   bool
 }
 
 const rotateCost = 1000
-const uint64max = ^uint64(0)
+const uintmax = ^uint(0)
 
 func isWalkable(x, y int, grid [][]byte) bool {
 	return x >= 0 && y >= 0 && y < len(grid) && x < len(grid[y]) && grid[y][x] != '#'
 }
 
 var d2dd = [4][2]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
-var dirCost = [3]uint64{rotateCost + 1, 1, rotateCost + 1}
+var dirCost = [3]uint{rotateCost + 1, 1, rotateCost + 1}
 
 // buildGraph builds a graph where each node is a path within the grid
 // without any "decision points" - that is, that path can be walked (possibly through corners)
@@ -37,16 +37,16 @@ var dirCost = [3]uint64{rotateCost + 1, 1, rotateCost + 1}
 // The node returned by any particular call to buildGraph is the node representing
 // the path that starts at the given coordinates and direction.
 // If the node would lead to a dead-end, this function returns nil.
-func buildGraph(cx, cy, d int, cost uint64, grid [][]byte, seen map[coord]*node) *node {
+func buildGraph(cx, cy, d int, cost uint, grid [][]byte, seen map[coord]*node) *node {
 	if v, ok := seen[coord{cx, cy, d}]; ok {
 		return v
 	}
-	n := &node{deadEnd: true}
+	n := &node{c: coord{cx, cy, d}}
 	seen[coord{cx, cy, d}] = n
 	for {
 		var walkable [3]bool
 		if grid[cy][cx] == 'E' {
-			n.cost, n.dcost, n.end, n.deadEnd = cost, uint64max, true, false
+			n.cost, n.dcost, n.end = cost, uintmax, true
 			return n
 		}
 		wc, wi := 0, 0
@@ -62,28 +62,28 @@ func buildGraph(cx, cy, d int, cost uint64, grid [][]byte, seen map[coord]*node)
 		} else if wc == 1 {
 			nd := (d + 4 + wi) & 3
 			ndd := d2dd[nd]
-			cx, cy, d, cost = cx+ndd[0], cy+ndd[1], nd, cost+dirCost[wi+1]
+			if wi == 0 {
+				cx, cy, d, cost = cx+ndd[0], cy+ndd[1], nd, cost+dirCost[wi+1]
+			} else {
+				n.cost, n.dcost = cost, uintmax
+				if ne := buildGraph(cx+ndd[0], cy+ndd[1], nd, 0, grid, seen); ne != nil {
+					n.neighbors[wi+1] = ne
+				} else {
+					return nil
+				}
+				return n
+			}
 		} else {
-			n.cost, n.dcost, n.deadEnd = cost, uint64max, false
+			n.cost, n.dcost = cost, uintmax
 			for i := 0; i < 3; i++ {
 				if !walkable[i] {
 					continue
 				}
 				nd := (d + 4 + i - 1) & 3
 				ndd := d2dd[nd]
-				if ne := buildGraph(cx+ndd[0], cy+ndd[1], nd, 0, grid, seen); ne != nil && !ne.deadEnd {
+				if ne := buildGraph(cx+ndd[0], cy+ndd[1], nd, 0, grid, seen); ne != nil {
 					n.neighbors[i] = ne
 				}
-			}
-			allDeadEnd := true
-			for i := 0; i < 3 && allDeadEnd; i++ {
-				if n.neighbors[i] != nil && !n.neighbors[i].deadEnd {
-					allDeadEnd = false
-				}
-			}
-			if allDeadEnd {
-				n.deadEnd = true
-				return nil
 			}
 			return n
 		}
@@ -110,33 +110,62 @@ func (pq *priorityQueue) Pop() interface{} {
 	return x
 }
 
-func dijkstra(start *node) uint64 {
+func dijkstra(start *node) (uint, uint64) {
 	start.dcost = start.cost
-	pq, seen := make(priorityQueue, 0, 8), make(map[*node]struct{})
+	var pq priorityQueue
 	heap.Push(&pq, start)
+	minCostToEnd := uintmax
+	var ends []*node
 	for pq.Len() > 0 {
 		n := heap.Pop(&pq).(*node)
 		if n.end {
-			return n.dcost
-		}
-		if _, ok := seen[n]; ok {
+			if n.dcost < minCostToEnd {
+				ends = append(ends, n)
+				minCostToEnd = n.dcost
+			}
 			continue
 		}
-		seen[n] = struct{}{}
+		n.seen = true
 		for i, ne := range n.neighbors {
-			if ne == nil || ne.deadEnd {
+			if ne == nil || ne.seen {
 				continue
 			}
-			if _, ok := seen[ne]; ok {
-				continue
-			}
-			if n.dcost+ne.cost+dirCost[i] < ne.dcost {
-				ne.dcost = n.dcost + ne.cost + dirCost[i]
+			c := n.dcost + ne.cost + dirCost[i]
+			if c < ne.dcost {
+				ne.dcost = c
+				ne.prev = []*node{n}
 				heap.Push(&pq, ne)
+			} else if c == ne.dcost {
+				ne.prev = append(ne.prev, n)
 			}
 		}
 	}
-	panic("should not happen")
+	return minCostToEnd, computeNumberOfDistinctPointsVisited(ends[0])
+}
+
+func computeNumberOfDistinctPointsVisited(end *node) uint64 {
+	var count uint64
+	var queue []*node
+	queue = append(queue, end)
+	seen := make(map[coord]struct{})
+	for len(queue) > 0 {
+		n := queue[0]
+		queue = queue[1:]
+		// compute all positions that can be reached from this node
+		nd := d2dd[n.c.d]
+		for i := 0; i < int(n.cost)+1; i++ {
+			nx, ny := n.c.x+nd[0]*i, n.c.y+nd[1]*i
+			if _, ok := seen[coord{nx, ny, 0}]; ok {
+				continue
+			}
+			seen[coord{nx, ny, 0}] = struct{}{}
+			count++
+		}
+		for _, ne := range n.prev {
+			queue = append(queue, ne)
+		}
+	}
+	return count
 }
 
 func input() ([][]byte, int, int) {
@@ -157,12 +186,11 @@ func input() ([][]byte, int, int) {
 func main() {
 	grid, sx, sy := input()
 	seen := make(map[coord]*node)
-	// part 1
-	{
-		time1 := time.Now()
-		start := buildGraph(sx, sy, 0, 0, grid, seen)
-		result := dijkstra(start)
-		time2 := time.Now()
-		fmt.Printf("Part1 (in %v): %v\n", time2.Sub(time1), result)
-	}
+	time1 := time.Now()
+	start := buildGraph(sx, sy, 0, 0, grid, seen)
+	result, uniquePoints := dijkstra(start)
+	time2 := time.Now()
+	fmt.Printf("Part1: %v\n", result)
+	fmt.Printf("Part2: %v\n", uniquePoints)
+	fmt.Printf("Total time: %v\n", time2.Sub(time1))
 }
